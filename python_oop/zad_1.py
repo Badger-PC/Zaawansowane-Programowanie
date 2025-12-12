bb5a0634be08615d7616f149099944020b56161f
Mariusz Borczuk <mariusz.borczuk@edu.uekat.pl>
2025-12-12 21:22:21 +0100

    ruff and pycodestyle pep8 validation ci github action

--- python_oop/zad_1.py
+++ python_oop/zad_1.py
@@ -5,24 +5,28 @@ stworzyć 2 przykładowe obiekty klasy, tak aby dla pierwszego obiektu metoda
 zwracała true , a dla drugiego false .
 """
 
+
 class Student:
-	def __init__(self, name: str, marks: list[float]):
-		self.name: str = name
-		self.marks: list[float] = [float(m) for m in marks]
+    def __init__(self, name: str, marks: list[float]):
+        self.name: str = name
+        self.marks: list[float] = [float(m) for m in marks]
 
-	def average(self) -> float:
-		if not self.marks:
-			return 0.0
-		return sum(self.marks) / len(self.marks)
+    def average(self) -> float:
+        if not self.marks:
+            return 0.0
+        return sum(self.marks) / len(self.marks)
 
-	def is_passed(self) -> bool:
-		return self.average() > 50
+    def is_passed(self) -> bool:
+        return self.average() > 50
 
 
 if __name__ == "__main__":
-	student_pass = Student("Mariusz", [60, 70, 40])  
-	student_fail = Student("Marek", [40, 50])   
+    student_pass = Student("Mariusz", [60, 70, 40])
+    student_fail = Student("Marek", [40, 50])
 
-	print(f"{student_pass.name} średnia = {student_pass.average():.2f}, zdane? {student_pass.is_passed()}")
-	print(f"{student_fail.name} średnia = {student_fail.average():.2f}, passed? {student_fail.is_passed()}")
-
+    print(
+        f"{student_pass.name} średnia = {student_pass.average():.2f}, zdane? {student_pass.is_passed()}"
+    )
+    print(
+        f"{student_fail.name} średnia = {student_fail.average():.2f}, passed? {student_fail.is_passed()}"
+    )
